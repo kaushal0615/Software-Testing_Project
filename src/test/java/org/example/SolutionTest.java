f72d054d45ed533589bd8ae84dd960ae81b01378
Kaushigithub <kypancholi2@gmail.com>
2024-11-18 19:16:32 +0530

    first example

---
+++ src/test/java/org/example/SolutionTest.java
@@ -0,0 +1,35 @@
+    package org.example;
+
+    import org.junit.jupiter.api.Test;
+    import static org.junit.jupiter.api.Assertions.*;
+
+    class SolutionTest {
+
+        Solution solution = new Solution();
+
+        @Test
+        void testDecryptWithKZero() {
+            assertArrayEquals(new int[]{0, 0, 0, 0}, solution.decrypt(new int[]{1, 2, 3, 4}, 0));
+        }
+
+        @Test
+        void testDecryptWithPositiveK() {
+            assertArrayEquals(new int[]{5, 7, 5, 3}, solution.decrypt(new int[]{1, 2, 3, 4}, 2));
+        }
+
+        @Test
+        void testDecryptWithNegativeK() {
+            assertArrayEquals(new int[]{7, 5, 3, 5}, solution.decrypt(new int[]{1, 2, 3, 4}, -2));
+        }
+
+        @Test
+        void testDecryptWithEmptyArray() {
+            assertArrayEquals(new int[]{}, solution.decrypt(new int[]{}, 1));
+        }
+
+        @Test
+        void testDecryptWithSingleElementArray() {
+            assertArrayEquals(new int[]{42}, solution.decrypt(new int[]{42}, 1));
+        }
+    }
+
