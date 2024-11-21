41d815c162c327f08186d1b252137a8f3f97adce
Kaushigithub <kypancholi2@gmail.com>
2024-11-21 18:17:11 +0530

    j

--- src/main/java/org/example/CanArrange.java
+++ src/main/java/org/example/CanArrange.java
@@ -13,7 +13,6 @@ public class CanArrange {
         if(freq[0] % 2 != 0){
             return false;
         }
-
         for(int i = 1;i <= k / 2;i++){
             if(freq[i] != freq[k-i]) {
                 return false;
